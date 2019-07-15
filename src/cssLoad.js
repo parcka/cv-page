9cd87d36dd39adb9564070c71b8fe5962427ecb1
Parcka <rmanchon@hotmail.com>
2019-07-15 18:57:34 -0400

    fix: webpack-config

---
+++ src/cssLoad.js
@@ -0,0 +1,8 @@
+// import './css/bootstrap/bootstrap.min.css';
+// import './css/font-awesome/css/font-awesome.min.css';
+// import './css/jquery.mCustomScrollbar.min.css';
+// import './css/owl-carousel/owl.carousel.min.css';
+// import './css/owl-carousel/owl.theme.default.min.css';
+// import './css/magnific-popup/magnific-popup.css';
+// import './css/style.css';
+// import './css/responsive.css';
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,78 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
 
 module.exports = {
-    mode: 'Production',
+    mode: 'production',
     entry: {
         app: './src/index.js'
     },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.[contentHash].js'
+    },
+    // context: path.resolve(__dirname, './src/*.*'),
+    module: {
+        rules: [{
+                test: /\.js$/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+            },
+            {
+                test: /.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)$/,
+                loader: 'url-loader?limit=10000',
+            },
+            {
+                test: /\.css$/,
+                use: [{
+                        loader: MiniCssExtractPlugin.loader,
+
+                    }, {
+                        loader: 'css-loader',
+                        options: {
+                            // url: false
+                        },
+                    },
+
+                ],
+            },
+
+
+
+        ]
+    },
+
+
+
     plugins: [
         // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            title: 'Production'
-        })
+
+            template: './src/index.html'
+
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+        }),
+
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jquery: "jquery",
+            "windows.jQuery": "jquery",
+            jQuery: "jquery",
+        }),
+
+
     ],
     output: {
         filename: '[name].bundle.js',
